52c09410d1e7f577fb09d4b8fc9c8cf4993ca393
rjanhealth <76500554+rjanhealth@users.noreply.github.com>
2021-03-08 18:19:28 +1100

    Update to standardise LGA case

--- scripts/extract_data.R
+++ scripts/extract_data.R
@@ -10,23 +10,21 @@ setwd("E:/PBIX/NCoronavirus 2020/Stata nCoV reporting/31 Azure Data Model/DART/D
 a <- fread("labresults.txt",sep=",")
 b <- fread("Persons.txt",sep=",")
 # join dataframes
-c <- merge(x = a, y = b[ , c("RecordID", "State", "MetroRural", "LGA", "AddressLine1", "AddressLine2", "FullCleanAddress", "StreetName", "Suburb","Postcode", "RawPostcode", "Latitude",  "Longitude")], by = "RecordID", all.x=TRUE)
-# serology is excluded as it is a retrospective test that checks for the presence of antibodies
-d <- filter(c, TestType != 'Serology')
+c <- merge(x = a, y = b[ , c("RecordID", "TestType", "LGA", "FullCleanAddress", "AddressLine1", "AddressLine2", "StreetName", "Suburb", "State", "Postcode", "RawPostcode", "Latitude", "Longitude")], by = "RecordID", all.x=TRUE)
 # check df for NA's
-d %>%
+c %>%
   summarise_all(funs(sum(is.na(.))))
 # remove columns with NA's for latitude and/or longitude
 # this will minimise our extract size but some of these have a valid address/postcode with no coordinates populated
-e <-d %>%
+d <-c %>%
   filter(!is.na(Latitude|Longitude))
 # select columns wanted
-f <- select(e, "TestDate", "RecordID", "State", "MetroRural", "LGA", "AddressLine1", "AddressLine2", "FullCleanAddress", "StreetName", "Suburb", "Postcode", "RawPostcode", "Latitude",  "Longitude")
+e <- select(d, "TestDate", "RecordID", "TestType", "LGA", "FullCleanAddress", "AddressLine1", "AddressLine2", "StreetName", "Suburb", "State", "Postcode", "RawPostcode", "Latitude", "Longitude")
 # chnage to date format
-f$TestDate <- as.Date(f$TestDate, '%d/%m/%Y')
+e$TestDate <- as.Date(e$TestDate, '%d/%m/%Y')
 # filter to only include feb 2021 onwards
-g <- filter(f, TestDate > "2021-01-31")
-#export .csv file
+f <- filter(e, TestDate > "2021-01-31")
+#export .csv files
 setwd("C:/Users/rjan2103/Documents")
-fwrite(g, "sitrep.csv", sep=",")
+fwrite(f, "sitrep.csv", sep=",")
 #fwrite(g, "latest.csv", sep=",")
