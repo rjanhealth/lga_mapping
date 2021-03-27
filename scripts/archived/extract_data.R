df8e48b236f960743770e01bfba0ac944cc2c047
rjanhealth <76500554+rjanhealth@users.noreply.github.com>
2021-03-27 13:11:23 +1100

    update

---
+++ scripts/archived/extract_data.R
@@ -0,0 +1,31 @@
+# load packages
+library(data.table)
+library(dplyr)
+options(warn=-1)
+# set wd to selected cut
+setwd("E:/PBIX/NCoronavirus 2020/Stata nCoV reporting/31 Azure Data Model/DART/Data snapshots/sitrep")
+#setwd("E:/PBIX/NCoronavirus 2020/Stata nCoV reporting/31 Azure Data Model/DART/Data snapshots/latest")
+# read in data
+a <- fread("labresults.txt",sep=",")
+b <- fread("Persons.txt",sep=",")
+# join dataframes
+c <- merge(x = a, y = b[ , c("RecordID", "LGA", "MetroRural", "FullCleanAddress", "AddressLine1", "AddressLine2", "StreetName", "Suburb", "State", "Postcode", "RawPostcode", "Latitude", "Longitude")], by = "RecordID", all.x=TRUE)
+# check df for NA's
+c %>%
+  summarise_all(funs(sum(is.na(.))))
+# remove columns with NA's for latitude and/or longitude
+# this will minimise our extract size but some of these have a valid address/postcode with no coordinates populated
+d <-c %>%
+  filter(!is.na(Latitude|Longitude))
+# select columns wanted
+e <- select(d, "TestDate", "RecordID", "TestType", "LGA", "MetroRural", "AddressLine1", "AddressLine2", "FullCleanAddress", "StreetName", "Suburb", "State", "Postcode", "RawPostcode", "Latitude", "Longitude")
+# change to date format
+e$TestDate <- as.Date(e$TestDate, '%d/%m/%Y')
+# filter date range
+f <- filter(e, TestDate > "2021-01-31")
+#convert column names to lower
+names(f)<-tolower(names(f))
+#export .csv files
+setwd("C:/Users/rjan2103/Documents")
+fwrite(f, "sitrep.csv", sep=",")
+#fwrite(g, "latest.csv", sep=",")
